4237055174dd0396199132f80716089f484232c8
Harry Cordewener <admin@twilightdays.org>
2024-10-26 19:50:22 -0500

    TUnit-Test - start setup.

---
+++ TUnit-Test/Tests.cs
@@ -0,0 +1,21 @@
+﻿namespace TUnit_Test
+{
+	public class Tests
+	{
+		[Before(Test)]
+		public async Task Setup()
+		{
+			/* var container = new ArangoDbBuilder()
+				.WithImage("arangodb:latest")
+				.WithPassword("password")
+				.Build();*/
+			await Task.CompletedTask;
+		}
+
+		[Test]
+		public async Task Test1()
+		{
+			await Assert.That(true).IsTrue();
+		}
+	}
+}
